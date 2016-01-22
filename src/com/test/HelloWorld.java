6522df324fc8a7c588d6db79ed3b867eaeaa0a3e
OlaYang <2785739797@qq.com>
2016-01-22 16:17:39 +0800

    首次提交到本地仓库

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,21 @@
+/**   
+* @Title: HelloWorld.java 
+* @Package com.test 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author yangyong
+* @date 2016年1月22日 下午4:11:49 
+* @version V1.0   
+*/
+package com.test;
+
+/** 
+* @ClassName: HelloWorld 
+* @Description: TODO(这里用一句话描述这个类的作用) 
+* @author yangyong
+* @date 2016年1月22日 下午4:11:49 
+*  
+*/
+public class HelloWorld
+{
+
+}
